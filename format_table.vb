8c1b3ec4d5673b902ed633222925d681b2928af3
root <maxim.litvak@gmail.com>
2014-11-26 23:06:24 +0100

    borders packed in loops

--- format_table.vb
+++ format_table.vb
@@ -41,122 +41,71 @@ QuellRange.Select
 End Sub
 
 Sub FormatBody(Selection)
+
+Dim xlBorders(6) as Integer
+xlBorders = Array(xlEdgeLeft, xlEdgeTop, xlEdgeBottom, xlEdgeRight, xlInsideVertical, xlInsideHorizontal)
+
 Selection.Borders(xlDiagonalDown).LineStyle = xlNone
 Selection.Borders(xlDiagonalUp).LineStyle = xlNone
-
-	With Selection.Borders(xlEdgeLeft) ' this block draws the left border of selection
+For Each xlBorder in xlBorders
+	With Selection.Borders(xlBorder) ' this block draws the left border of selection
 		.LineStyle = xlContinuous
 		.Weight = xlThin
 		.ColorIndex = xlAutomatic
 	End With
-
-	With Selection.Borders(xlEdgeTop) ' this block draws the top border of selection
-		.LineStyle = xlContinuous
-		.Weight = xlThin
-		.ColorIndex = xlAutomatic
-	End With
-
-	With Selection.Borders(xlEdgeBottom) ' this block draws the bottom border of selection
-		.LineStyle = xlContinuous
-		.Weight = xlThin
-		.ColorIndex = xlAutomatic
-	End With
-
-	With Selection.Borders(xlEdgeRight) ' this block draws the right border of selection
-		.LineStyle = xlContinuous
-		.Weight = xlThin
-		.ColorIndex = xlAutomatic
-	End With
-
-	With Selection.Borders(xlInsideVertical) ' this block puts the vertical lines inside selection
-		.LineStyle = xlContinuous
-		.Weight = xlThin
-		.ColorIndex = xlAutomatic
-	End With
-
-	With Selection.Borders(xlInsideHorizontal) ' this block puts the horizontal lines inside selection
-		.LineStyle = xlContinuous
-		.Weight = xlThin
-		.ColorIndex = xlAutomatic
-	End With
-
+Next xlBorder
 End Sub
 
 Sub FormatHeader(FirstRow, FirstCol, LastColOrRow, TypeOfHeader)
 If TypeOfHeader = "Column" Then
-	Range(ActiveSheet.Cells(FirstRow, FirstCol), ActiveSheet.Cells(LastRowOrCol, FirstCol)).Select ' The header is in the row (i.e. horizontal)
+	Range(ActiveSheet.Cells(FirstRow, FirstCol), ActiveSheet.Cells(LastColOrRow, FirstCol)).Select ' The header is in the row (i.e. horizontal)
 Else 'Row
-	Range(ActiveSheet.Cells(FirstRow, FirstCol), ActiveSheet.Cells(FirstRow, LastRowOrCol)).Select ' The header is in the column (i.e. vertical)
+	Range(ActiveSheet.Cells(FirstRow, FirstCol), ActiveSheet.Cells(FirstRow, LastColOrRow)).Select ' The header is in the column (i.e. vertical)
 End If
 
 	Selection.Borders(xlDiagonalDown).LineStyle = xlNone
 	Selection.Borders(xlDiagonalUp).LineStyle = xlNone
 
-	With Selection.Borders(xlEdgeLeft)
+Dim xlBorders(4) as Integer
+xlBorders = Array(xlEdgeLeft, xlEdgeTop, xlEdgeBottom, xlEdgeRight)
+
+For Each xlBorder in xlBorders
+	With Selection.Borders(xlBorder)
 		.LineStyle = xlContinuous
 		.Weight = xlMedium
 		.ColorIndex = xlAutomatic
 	End With
+Next xlBorder
 
-	With Selection.Borders(xlEdgeTop)
-		.LineStyle = xlContinuous
-		.Weight = xlMedium
-		.ColorIndex = xlAutomatic
-	End With
+Selection.Borders(xlInsideVertical).LineStyle=xlNone
 
-	With Selection.Borders(xlEdgeBottom)
-		.LineStyle = xlContinuous
-		.Weight = xlMedium
-		.ColorIndex = xlAutomatic
-	End With
+With Selection.Interior
+	.ColorIndex = 15
+	.Pattern = xlSolid
+End With
 
-	With Selection.Borders(xlEdgeRight)
-		.LineStyle = xlContinuous
-		.Weight = xlMedium
-		.ColorIndex = xlAutomatic
-	End With
-
-	Selection.Borders(xlInsideVertical).LineStyle=xlNone
-
-	With Selection.Interior
-		.ColorIndex = 15
-		.Pattern = xlSolid
-	End With
-	Selection.Font.Bold = True
+Selection.Font.Bold = True
 
 End Sub
 
 Sub FormatColumns(FirstCol, LastCol, FirstRow, LastRow)
+
+Dim xlBorders(4) as Integer
+xlBorders = Array(xlEdgeLeft, xlEdgeTop, xlEdgeBottom, xlEdgeRight)
+
 For i = FirstCol To LastCol
 	Range(ActiveSheet.Cells(FirstRow, i), ActiveSheet.Cells(LastRow, i)).Select
 	Selection.Borders(xlDiagonalDown).LineStyle = xlNone
 	Selection.Borders(xlDiagonalUp).LineStyle = xlNone
-
-	With Selection.Borders(xlEdgeLeft)
-		.LineStyle = xlContinuous
-		.ColorIndex = 0
-		.Weight = xlMedium
-	End With
-
-	With Selection.Borders(xlEdgeTop)
-		.LineStyle = xlContinuous
-		.ColorIndex = 0
-		.Weight = xlMedium
-	End With
-
-	With Selection.Borders(xlEdgeBottom)
-		.LineStyle = xlContinuous
-		.ColorIndex = 0
-		.Weight = xlMedium
-	End With
-
-	With Selection.Borders(xlEdgeRight)
-		.LineStyle = xlContinuous
-		.ColorIndex = 0
-		.Weight = xlMedium
-	End With
-
+	For Each xlBorder in xlBorders
+		With Selection.Borders(xlBorder)
+			.LineStyle = xlContinuous
+			.ColorIndex = 0
+			.Weight = xlMedium
+		End With
+	Next xlBorder
 	Selection.Borders(xlInsideVertical).LineStyle = xlNone
 	Selection.Columns.AutoFit
+Next i
 
 End Sub
